f10c272bf9c8071d255a8df71116d90656a80290
“faizanrajput1120” <Faizanrajput488@gmail.com>
2025-09-08 02:01:36 +0500

    Initial Project

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::prefix('ERPLive')->group(function () {
     Route::get('', function () {
         return view('full-width-light.index');
     });
-    Route::get('migrate',[  LevelController::class,'index'])->name('migrate');
+    Route::get('migrate',[  ManageMigration::class,'index'])->name('migrate');
     // Custom print route (always place BEFORE resource)
     Route::get('levels/print', [LevelController::class, 'printTree'])->name('levels.print');
 
