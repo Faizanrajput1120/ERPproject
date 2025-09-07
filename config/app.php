6891862eca5a95778a8d46e927823f9dbf59e020
“faizanrajput1120” <Faizanrajput488@gmail.com>
2025-09-08 04:12:18 +0500

    Update Login and Register

--- config/app.php
+++ config/app.php
@@ -57,7 +57,7 @@ return [
 
     'url' => env('APP_URL', 'https://real-erp.net/ERPLive'),
 
-    'asset_url' => env('ASSET_URL'),
+    'asset_url' => env('https://real-erp.net/ERPLive'),
 
     /*
     |--------------------------------------------------------------------------
