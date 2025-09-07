735a25c4807789e7a89f6d0f2f9675026526658b
“faizanrajput1120” <Faizanrajput488@gmail.com>
2025-09-08 01:06:06 +0500

    Initial Project

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,20 @@ use App\Http\Controllers\LevelController;
 use App\Http\Controllers\ChartOfAccountController;
 use Illuminate\Support\Facades\Route;
 
-// Home route
-Route::get('', function () {
-    return view('full-width-light.index');
+Route::prefix('prbox')->group(function () {
+
+    // Home route
+    Route::get('', function () {
+        return view('full-width-light.index');
+    });
+
+    // Custom print route (always place BEFORE resource)
+    Route::get('levels/print', [LevelController::class, 'printTree'])->name('levels.print');
+
+    // Resource route (skip show if not needed)
+    Route::resource('levels', LevelController::class)->except(['show']);
+
+    // Other resource routes
+    Route::resource('chart-of-accounts', ChartOfAccountController::class);
+
 });
-
-// Custom print route (always place BEFORE resource)
-Route::get('levels/print', [LevelController::class, 'printTree'])->name('levels.print');
-
-// Resource route (skip show if not needed)
-Route::resource('levels', LevelController::class)->except(['show']);
-
-// Other resource routes
-Route::resource('chart-of-accounts', ChartOfAccountController::class);
